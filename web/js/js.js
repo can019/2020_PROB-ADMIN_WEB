79f5f97a0fe10fbabd8cfd1d2588b00a27d07aae
can019 <jys01012@gmail.com>
2020-07-01 14:41:24 +0900

    add rows/delete rows/ switch option changed in [function] calAvg

--- web/js/js.js
+++ web/js/js.js
@@ -28,6 +28,7 @@ function calAvg(element){
     }
     else{
         let num = 0;
+        let non = 0;
         for(let cursor = 0;cursor<element.length;cursor++){
             switch(element[cursor].innerHTML.trim()){
                 case 'A+': num+=4.5;
@@ -46,11 +47,30 @@ function calAvg(element){
                     break;
                 case'D': num+=1;
                     break;
-                default :
+                case  'F' : num+=0;
                     break;
+                default : non++;
+                    break;
+
             }
         }
-        return num/element.length;
+        return num/(element.length-non);
     }
 
 }
+
+function add_row() {
+    var tbody_tbody1 = document.getElementById('tbody1');
+    var row = tbody_tbody1.insertRow( my_tbody.rows.length ); // 하단에 추가
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = '항목';
+    cell2.innerHTML = new Date().toUTCString();
+  }
+
+  function delete_row() {
+    var my_tbody = document.getElementById('my-tbody');
+    if (my_tbody.rows.length < 1) return;
+    // my_tbody.deleteRow(0); // 상단부터 삭제
+    my_tbody.deleteRow( my_tbody.rows.length-1 ); // 하단부터 삭제
+  }
