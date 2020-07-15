7582d70991297fb33c0a1b9ba862ed6c9a1a738e
can019 <jys01012@gmail.com>
2020-07-16 00:25:01 +0900

    blank fixed

--- web/webprogramming.php
+++ web/webprogramming.php
@@ -29,11 +29,14 @@
 
                     for($i=0;!trim(feof($fp));$i++){
                         $temp = trim(fgets($fp));
-                        $data[$i] = explode('|',trim($temp));
-                        echo'<tr class ="content">
-                        <td><input class = "chk" type="checkbox"></td><td class="stdname">',$data[$i][0],
-                        '</td><td class="number">',$data[$i][1],'</td><td class="age">',$data[$i][2],'</td><td class = "grade">',$data[$i][3],
-                        '</td><td>',$data[$i][4],'</td>';                   
+                        if($temp!=''){
+                            $data[$i] = explode('|',trim($temp));
+                            echo
+                            '<tr class ="content">
+                            <td><input class = "chk" type="checkbox"></td><td class="stdname">',$data[$i][0],
+                            '</td><td class="number">',$data[$i][1],'</td><td class="age">',$data[$i][2],
+                            '</td><td class = "grade">',$data[$i][3],'</td><td>',$data[$i][4],'</td>';      
+                        }             
                     }
                 ?>
                 </tbody>
